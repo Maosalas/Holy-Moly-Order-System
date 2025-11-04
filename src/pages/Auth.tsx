89ad6d14d4ac60d69196f012e6bb968265e6d715
Mauricio Salas Steller <mao.salas.steller@gmail.com>
2025-11-04 16:37:19 -0600

    Changes

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -8,7 +8,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { useToast } from "@/hooks/use-toast";
-import logo from "@/assets/Basic Branding-01.png";
+import logo from "@/assets/Orderly-logo.png";
 import { organizationsApi } from "@/lib/api";
 
 const Auth = () => {
